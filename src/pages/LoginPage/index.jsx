6017ab2a9907ad72eafc7f3e998b8808d8d6c061
Leonardo Lucas <leonardolucas2020@gmail.com>
2022-05-06 18:45:18 -0400

    Adicionando tailwindcss a aplicação

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <h1>LoginPage</h1>;
+  return (
+    <div className="login">
+      <h1 className="title">Login do sistema</h1>
+      <form className="form"></form>
+    </div>
+  );
 };
 
 export default LoginPage;
