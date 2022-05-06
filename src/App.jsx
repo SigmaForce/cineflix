4e871e3374189d1b4e00f9df20bad025c95ac391
Leonardo Lucas <leonardolucas2020@gmail.com>
2022-05-06 18:31:23 -0400

    Limpando a aplicação e Criando as Primeiras Páginas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HomePage from "./pages/HomePage";
+
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <div className="app">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
